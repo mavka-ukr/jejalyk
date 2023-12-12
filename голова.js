0146f7e9586feb2c9d684875c51fd7ea0b284d9a
Davyd Kohut <davyd.kohut@gmail.com>
2023-12-12 02:27:42 +0200

    🌠

--- голова.js
+++ голова.js
@@ -529,15 +529,12 @@ function мЄ(a, b) {
   }
   var aMavkaValue = a[MAVKA];
   if (aMavkaValue) {
-    var bMavkaValue = b[MAVKA];
-    if (bMavkaValue) {
-      var structure = bMavkaValue;
-      while (structure) {
-        if (aMavkaValue === structure) {
-          return м_так;
-        }
-        structure = structure[MAVKA]?.parent;
+    var structure = b;
+    while (structure) {
+      if (aMavkaValue === structure) {
+        return м_так;
       }
+      structure = structure[MAVKA]?.parent;
     }
   }
   return м_ні;
